8111b510a804ca77577ab27b358db73f05f1f8c8
cw26378 <wangcheng@utexas.edu>
2018-07-23 22:11:27 -0700

    add README

---
+++ Assets/Scripts/CreateAgent.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateAgent : MonoBehaviour {
+    float height = 60.0f;
+    // Use this for initialization
+    public GameObject new_RVO_agent;
+    public GameObject new_agentAir;
+
+    private bool SpawnIsDone;
+    private bool IsGround;
+    private bool IsAir;
+
+    /*void Start()
+    {
+        SpawnIsDone = GameObject.FindGameObjectWithTag("Manager").GetComponent<UI_ButtonControl>().SpawnIsDone;
+        IsGround = GameObject.FindGameObjectWithTag("Manager").GetComponent<UI_ButtonControl>().ToAddGround;
+        IsAir = GameObject.FindGameObjectWithTag("Manager").GetComponent<UI_ButtonControl>().ToAddAir;
+    
+    }
+    */
+
+    // Update is called once per frame
+	void FixedUpdate () {
+        Vector3 spawnTarget;
+
+        SpawnIsDone = GameObject.FindGameObjectWithTag("Manager").GetComponent<UI_ButtonControl>().SpawnIsDone;
+        IsGround = GameObject.FindGameObjectWithTag("Manager").GetComponent<UI_ButtonControl>().ToAddGround;
+        IsAir = GameObject.FindGameObjectWithTag("Manager").GetComponent<UI_ButtonControl>().ToAddAir;
+
+        if(Input.GetMouseButtonDown(0) && SpawnIsDone == false){
+            Camera cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                spawnTarget = hit.point;
+
+                Debug.Log("hit.point is: "+ hit.point.ToString());
+
+                GameObject newAgent;
+                if (IsGround && IsAir)
+                {
+                    Debug.Log("Error! Cannot be air and ground at the same time...Please select one...");
+                    return;
+                }
+
+                else if (IsGround)
+                {
+                    if (AstarPath.active.GetNearest(hit.point).node.Walkable == false)
+                    {
+                        Debug.Log("Cannot deploy units on non-walkable region!");
+                        return;
+                    }
+
+                    Debug.Log("New Ground Unit Deployed!");
+                    newAgent = Instantiate(new_RVO_agent, new Vector3(spawnTarget.x, transform.position.y, spawnTarget.z), Quaternion.Euler(0, 0, 0)) as GameObject;
+                }
+                else if(IsAir){
+                    Debug.Log("New Air Unit Deployed");
+                    spawnTarget.y = height;
+                    newAgent = Instantiate(new_agentAir, new Vector3(spawnTarget.x, transform.position.y, spawnTarget.z), Quaternion.Euler(0, 0, 0)) as GameObject;
+                }
+                else{
+                    Debug.Log("Nothing is generated");
+                }
+                //newAgent.cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();;
+            }
+        }
+		
+	}
+}
---
+++ Assets/Scripts/UI_ButtonControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_ButtonControl : MonoBehaviour {
+
+    public bool SpawnIsDone = false;
+    public bool ToAddGround = false;
+    public bool ToAddAir = false;
+	
+	public void ChangeAddGround () {
+
+        ToAddGround = true;
+        ToAddAir = false;
+        SpawnIsDone = false;
+
+	}
+
+    public void ChangeAddAir()
+    {
+
+        ToAddAir = true;
+        ToAddGround = false;
+        SpawnIsDone = false;
+    }
+
+    public void ReadyForRally()
+    {
+
+        SpawnIsDone = true;
+        ToAddGround = false;
+        ToAddAir = false;
+        GameObject.Find("Canvas").GetComponentsInChildren<Button>()[1].interactable = false;
+        GameObject.Find("Canvas").GetComponentsInChildren<Button>()[2].interactable = false;
+    }
+}
